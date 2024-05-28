1e7a3f687a62342f6805a5dc6c8d5688cca053f7
Alex Smith <121595771+alex-smith-uwec@users.noreply.github.com>
2024-05-28 17:16:42 -0500

    Add files via upload

---
+++ holistic.py
@@ -0,0 +1,215 @@
+import pandas as pd
+import numpy as np
+
+def reviewer(dash):
+    # Drop specified columns
+    dash = dash.drop(columns=['Primary Name', 'Birthdate', 'Academic Plan 1 (Long Descr)'])
+    
+    # Rename specified columns
+    dash = dash.rename(columns={
+        'Admit Plan': 'Plan',
+        'Meeting Dt': 'Appt',
+        'HS GPA': 'GPA',
+        'ACT Reported MATH': 'Official_MACT',
+        'Self Reported ACT MATH': 'Self_MACT',
+        'Unproctored MFND': 'MFUND',
+        'Unproctored AALG': 'AALG',
+        'Unproctored TAG': 'TAG',
+        'Unproctored Test Dt': 'Unproc_date'
+    })
+    
+    # Add specified columns with default None values
+    dash['Expedited'] = None
+    dash['SEN'] = None
+    dash['MGPA'] = None
+    dash['MACT']=None
+    dash['unproc_MPT'] = None
+    dash['proc_MPT']=None
+    dash['MATHH'] = None
+    dash['MATHH_date'] = None
+    
+    # Reorder columns
+    columns_order = [
+        'Emplid', 'Preferred Name', 'Plan', 'Appt', 'Expedited', 'SEN', 'MGPA', 'GPA',
+        'MACT','Official_MACT', 'Self_MACT', 'SAT Reported MATH', 'Self Reported SAT MATH', 
+        'MFUND', 'AALG', 'TAG', 'unproc_MPT', 'Unproc_date', 'WPT MFND', 'WPT AALG', 
+        'WPT TAG', 'proc_MPT', 'Holistic Level', 'MATHH', 'MATHH_date'
+    ]
+    
+    dash = dash[columns_order]
+    
+    return dash
+
+
+
+
+
+def math_act(row):
+    """
+    Get Math ACT score from available sources: official, self-reported, or converted from Math SAT.
+
+    This function retrieves the Math ACT score from a given row of data. The score is extracted based on the following priority:
+    1. Official Math ACT score
+    2. Self-reported Math ACT score
+    3. Converted Math SAT score (reported)
+    4. Converted Math SAT score (self-reported)
+
+    Args:
+        row (pd.Series): A row of data containing the following fields:
+            - 'Official_MACT': Official Math ACT score
+            - 'Self_MACT': Self-reported Math ACT score
+            - 'SAT Reported MATH': Reported Math SAT score
+            - 'Self Reported SAT MATH': Self-reported Math SAT score
+
+    Returns:
+        float: The Math ACT score, either directly from ACT or converted from SAT. Returns np.nan if no score is available.
+    """
+    # SAT to ACT conversion dictionary
+    sat_to_act = {
+        800: 36, 790: 35, 780: 35, 770: 35, 760: 34, 750: 33, 740: 33, 730: 32, 720: 32, 
+        710: 31, 700: 30, 690: 30, 680: 29, 670: 28, 660: 28, 650: 27, 640: 27, 630: 27, 
+        620: 26, 610: 26, 600: 25, 590: 25, 580: 24, 570: 24, 560: 23, 550: 23, 540: 22, 
+        530: 21, 520: 20, 510: 19, 500: 18, 490: 18, 480: 17, 470: 17, 460: 17, 450: 16, 
+        440: 16, 430: 16, 420: 16, 410: 15, 400: 15, 390: 15, 380: 15, 370: 14, 360: 14, 
+        350: 14, 340: 13, 330: 13, 320: 13, 310: 12, 300: 12, 290: 11, 280: 11, 270: 10, 
+        260: 10
+    }
+
+    if not pd.isna(row['Official_MACT']):
+        return row['Official_MACT']
+    elif not pd.isna(row['Self_MACT']):
+        return row['Self_MACT']
+    elif not pd.isna(row['SAT Reported MATH']):
+        sat_math = row['SAT Reported MATH']
+        return sat_to_act.get(sat_math, np.nan)
+    else:
+        sat_math = row['Self Reported SAT MATH']
+        return sat_to_act.get(sat_math, np.nan)
+
+def act(df):
+    """
+    Fill in the 'MACT' column using the math_act function.
+
+    Args:
+        df (pd.DataFrame): A dataframe containing the columns 
+            'Official_MACT', 'Self_MACT', 'SAT Reported MATH', 'Self Reported SAT MATH'.
+
+    Returns:
+        pd.DataFrame: The dataframe with the 'MACT' column filled.
+    """
+    df['MACT'] = df.apply(math_act, axis=1)
+    df = df.drop(columns=['Official_MACT', 'Self_MACT', 'SAT Reported MATH', 'Self Reported SAT MATH'])
+    return df
+
+##Define a function F_mpt to return math placement level between 1 and 7
+def unproc_mpt(mfund, aalg, tag):
+    """
+    Determines the unproctored math placement level based on math fundamentals score (mfund),
+    advanced algebra score (aalg), and tag score (tag). Returns a placement level from 1 to 7.
+    
+    Parameters:
+    mfund (float): Math Fundamentals score (unproctored)
+    aalg (float): Advanced Algebra score (unproctored)
+    tag (float): Trig score. (unproctored)
+
+    Returns:
+    float: The unproctored math placement level, or NaN if mfund is not a number.
+    """
+    if np.isnan(mfund):
+        return np.nan
+    if mfund < 355:
+        return 1
+    elif mfund < 465 and aalg < 455:
+        return 2
+    elif mfund < 465:
+        return 3
+    elif aalg < 400:
+        return 4
+    elif aalg < 555:
+        return 5
+    elif aalg < 575:
+        return 6
+    elif aalg>575 and tag<555:
+       return 6
+    else:
+        return 7
+
+def proc_mpt(mfund, aalg, tag):
+    """
+    Determines the math placement level based on  proctored math fundamentals score (WPT MFND),
+    advanced algebra score (WPT AALG), and tag score (WPT TAG). Returns a placement level from 1 to 7.
+    
+    Parameters:
+    WPT MFND (float): Math Fundamentals score (proctored)
+    WPT AALG (float): Advanced Algebra score (proctored)
+    WPT TAG (float): Trig score (proctored)
+
+    Returns:
+    float: The proctored math placement level, or NaN if WPT MFUND is not a number.
+    """
+    if np.isnan(mfund):
+        return np.nan
+    if mfund < 355:
+        return 1
+    elif mfund < 465 and aalg < 455:
+        return 2
+    elif mfund < 465:
+        return 3
+    elif aalg < 400:
+        return 4
+    elif aalg < 555:
+        return 5
+    elif aalg < 575:
+        return 6
+    elif aalg>575 and tag<555:
+       return 6
+    else:
+        return 7
+
+def reviewer_with_mpt(df):
+    """
+    Fill in the 'unproc_MPT' and 'proc_MPT' columns using the unproc_mpt and proc_mpt functions, respectively.
+
+    Args:
+        df (pd.DataFrame): A dataframe containing the columns 'MFUND', 'AALG', 'TAG' for unproc_MPT,
+                           and 'WPT MFND', 'WPT AALG', 'WPT TAG' for proc_MPT.
+
+    Returns:
+        pd.DataFrame: The dataframe with the 'unproc_MPT' and 'proc_MPT' columns filled.
+    """
+    df['unproc_MPT'] = df.apply(lambda row: unproc_mpt(row['MFUND'], row['AALG'], row['TAG']), axis=1)
+    df['proc_MPT'] = df.apply(lambda row: proc_mpt(row['WPT MFND'], row['WPT AALG'], row['WPT TAG']), axis=1)
+    return df
+
+
+def expedite(reviewer, admit_plans):
+    # Create a dictionary from the admit_plans dataframe for lookup
+    admit_plan_dict = admit_plans.set_index('Admit Plan')['expedite (if conditions met)'].to_dict()
+    
+    def get_expedited_value(row):
+        # First stage of logic
+        if pd.notna(row['WPT MFND']):
+            return row['proc_MPT']
+        elif pd.isna(row['MACT']) and row['MFUND'] <= 355:
+            return 1
+        elif pd.isna(row['MACT']) and 356 <= row['MFUND'] <= 465 and row['AALG'] < 455 and row['GPA'] < 3:
+            return 2
+        elif row['MACT'] <= 15:
+            return 1
+        elif row['MACT'] <= 19 and row['GPA'] < 3:
+            return 2
+        # Second stage of logic
+        elif 24 <= row['MACT'] <= 27 and admit_plan_dict.get(row['Plan'], 0) == 1:
+            return 5
+        elif 28 <= row['MACT'] <= 31 and admit_plan_dict.get(row['Plan'], 0) == 1:
+            return 6
+        # Third stage of logic
+        elif row['MACT'] >= 32 and row['GPA'] >= 3:
+            return 7
+        else:
+            return None
+    
+    # Apply the logic to each row in the reviewer dataframe
+    reviewer['Expedited'] = reviewer.apply(get_expedited_value, axis=1)
+    
+    return reviewer
---
+++ holistic2.py
@@ -0,0 +1,210 @@
+
+##this file has functions to take Reviewer with SEN and MGPA entered by humans and processing MATHH results
+import pandas as pd
+import numpy as np
+## gpa is overall gradepoint average (capped at 4)
+## mgpa is math gpa calculated during transcript review
+## mact is math ACT
+## mpt is math placement level as determined by mfund, aalg, tag
+## sen is assessment of senior year math achievement. Ranges from 0 to 10. Determined during transcript review. SEE RUBRIC!!
+
+def H_none(gpa, mgpa, sen):
+    """
+    Determines MATHH with no ACT, no MPT. Uses mgpa and sen from transcript review.
+    
+    Parameters:
+    gpa (float): Overall gradepoint average (capped at 4).
+    mgpa (float): Math GPA calculated during transcript review.
+    sen (int): Assessment of senior year math achievement (0-10).
+    
+    Returns:
+    int: MATHH level.
+    """
+    cap_gpa=min(gpa,4)
+    # agpa = (cap_gpa + mgpa) / 2
+    #geometric mean
+    agpa = (cap_gpa*mgpa)**0.5
+
+    if sen >= 8:
+        if agpa >= 3.5:
+            return 7
+        elif agpa >= 3.25:
+            return 6
+        elif agpa >= 3:
+            return 5
+        elif agpa >= 2.5:
+            return 4
+        else:
+            return 3
+    elif sen >= 5 and sen < 8:
+        if agpa >= 3.5:
+            return 6
+        elif agpa >= 3.25:
+            return 5
+        elif agpa >= 2.5:
+            return 4
+        else:
+            return 3
+    elif sen >= 3 and sen < 5:
+        if agpa > 3.5:
+            return 5
+        elif agpa >= 3:
+            return 4
+        elif agpa >= 2.5:
+            return 3
+        else:
+            return 2
+    else:  # sen < 3
+        if agpa > 3.5:
+            return 4
+        elif agpa >= 3:
+            return 3
+        elif agpa >= 2.5:
+            return 2
+        else:
+            return 1
+
+def H_mpt(gpa, mgpa, mpt, sen):
+    """
+    Determines MATHH with additional info of MPT only.
+    
+    Parameters:
+    gpa (float): Overall gradepoint average (capped at 4).
+    mgpa (float): Math GPA calculated during transcript review.
+    mpt (int): Math placement level.
+    sen (int): Assessment of senior year math achievement (0-10).
+    
+    Returns:
+    int: MATHH level.
+    """
+    h0 = H_none(gpa, mgpa, sen)
+
+    if mpt <= h0 - 2:
+        return h0-1 
+    elif mpt <= h0:
+        return h0
+    else:
+        return min(h0 + 1, 7)
+
+def H_mact(gpa, mgpa, mact, sen):
+    """
+    Determines MATHH with additional info of MACT only.
+    
+    Parameters:
+    gpa (float): Overall gradepoint average (capped at 4).
+    mgpa (float): Math GPA calculated during transcript review.
+    mact (int): Math ACT score.
+    sen (int): Assessment of senior year math achievement (0-10).
+    
+    Returns:
+    int: MATHH level.
+    """
+    h0 = H_none(gpa, mgpa, sen)
+
+    if h0 >= 4:  
+        return h0
+    else:
+        if mact >= 23:
+            return 5 ##propose changing this to 4, May 28, 2024
+        elif mact == 22:
+            return 4
+        elif mact == 21:
+            return 3
+        elif mact > 16:
+            return 2
+        else:
+            return 1
+
+def H_both(gpa, mgpa, mact, mpt, sen):
+    """
+    Determines MATHH with additional info of both MACT and unproctored MPT.
+    
+    Parameters:
+    gpa (float): Overall gradepoint average (capped at 4).
+    mgpa (float): Math GPA calculated during transcript review.
+    mact (int): Math ACT score.
+    mpt (int): Math placement level.
+    sen (int): Assessment of senior year math achievement (0-10).
+    
+    Returns:
+    int: MATHH level.
+    """
+    value2 = H_none(gpa, mgpa, sen)
+    value3 = H_mpt(gpa, mgpa, mpt, sen)
+    value6 = H_mact(gpa, mgpa, mact, sen)
+    return max(value2, value3, value6)
+
+
+
+
+
+def type(review):
+    conditions = [
+        review['proc_MPT'].notna(),
+        review['Expedited'].notna(),
+        review['MACT'].isna() & review['unproc_MPT'].isna(),
+        review['MACT'].isna() & review['unproc_MPT'].notna(),
+        review['MACT'].notna() & review['unproc_MPT'].isna(),
+        review['MACT'].notna() & review['unproc_MPT'].notna()
+    ]
+    choices = [
+        'h_proctored',
+        'h_expedited',
+        'h_none',
+        'h_mpt',
+        'h_act',
+        'h_both'
+    ]
+    review['Review Type'] = np.select(
+        conditions,
+        choices,
+        default=None
+    )
+    return review
+
+
+
+
+
+def mathh(df):
+    def apply_logic(row):
+        if row['Review Type'] == 'h_proctored':
+            return row['proc_MPT']
+        elif row['Review Type'] == 'h_expedited':
+            return row['Expedited']
+        elif row['Review Type'] == 'h_none':
+            return H_none(row['GPA'], row['MGPA'], row['SEN'])
+        elif row['Review Type'] == 'h_mpt':
+            return H_mpt(row['GPA'], row['MGPA'], row['unproc_MPT'], row['SEN'])
+        elif row['Review Type'] == 'h_act':
+            return H_mact(row['GPA'], row['MGPA'], row['MACT'], row['SEN'])
+        elif row['Review Type'] == 'h_both':
+            return H_both(row['GPA'], row['MGPA'], row['MACT'], row['unproc_MPT'], row['SEN'])
+        else:
+            return None
+
+    # Ensure MATHH_date is in datetime format
+    df['MATHH_date'] = pd.to_datetime(df['MATHH_date'], errors='coerce')
+    
+    # Create a copy of the original MATHH column to compare old and new values
+    original_mathh = df['MATHH'].copy()
+
+    # Apply logic to update MATHH column
+    df['MATHH'] = df.apply(apply_logic, axis=1)
+    
+    # Get the current date
+    current_date = pd.Timestamp.today().normalize()
+
+    # Update MATHH_date based on changes in MATHH values
+    for i in df.index:
+        if df.at[i, 'MATHH'] != original_mathh.at[i]:
+            # If MATHH has changed, update to today's date
+            df.at[i, 'MATHH_date'] = current_date
+
+    return df
+
+
+
+
+
+
